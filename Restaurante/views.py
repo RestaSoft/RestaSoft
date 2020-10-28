59d5905c6489d9773db3c2879ccdc35ce2b39516
Alejandro Pizano <alex.pizano1234@gmail.com>
2020-10-28 11:27:57 -0700

    Agregando URLS de pantallas

--- Restaurante/views.py
+++ Restaurante/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render, HttpResponse
 
-def LogIn(request):
+def logIn(request):
     return HttpResponse("Pantalla de inicio de Sesion")
+
+
+def home(request):
+    return HttpResponse("Bienvenida Socio")
+
+
+
+def altaProducto(request):
+    return HttpResponse("Pantalla Alta Productos")
+
+
+def inventario(request):
+    return HttpResponse("Inventario")
+
+
+def editarProducto(request):
+    return HttpResponse("Editar productos")
