ab3f59f997ecdde6f206d1033a0d3cb322f7b605
Alejandro Pizano <alex.pizano1234@gmail.com>
2020-10-28 10:42:55 -0700

    Inicio de Sesion en Views

--- Restaurante/urls.py
+++ Restaurante/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Restaurante.views import home
+from Restaurante.views import LogIn
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home/', LogIn),
 
 ]
