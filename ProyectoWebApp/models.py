534f1d29c953e728f38808ab0a13f8286e5a73b7
Brenda Gutiérrez <blilianagr@gmail.com>
2020-10-30 09:21:40 -0700

    Modificacion del modelo

--- ProyectoWebApp/models.py
+++ ProyectoWebApp/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Staffs(models.Model):
-    staff_id=models.IntegerField()
     user_id=models.IntegerField()
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
@@ -14,11 +13,9 @@ class Staffs(models.Model):
 
 
 class Permission(models.Model):
-    permission_id=models.IntegerField()
     name_permission=models.CharField(max_length=50)
 
 class Stores(models.Model):
-    stores_id=models.IntegerField()
     store_name=models.CharField(max_length=100)
     phone=models.IntegerField()
     email=models.EmailField()
@@ -28,26 +25,22 @@ class Stores(models.Model):
     zip_code=models.IntegerField()
 
 class Products(models.Model):
-    products_id=models.IntegerField()
     product_name=models.CharField(max_length=50)
     category_id=models.IntegerField()
     list_price=models.IntegerField()
     supplier_id=models.IntegerField()
 
 class Orders(models.Model):
-    order_id=models.IntegerField()
     staff_id=models.IntegerField()
     order_status=models.IntegerField()
     order_date=models.DateField()
 
 class Order_items(models.Model):
-    item_id=models.IntegerField()
     order_id=models.IntegerField()
     quantity=models.IntegerField()
     list_price=models.IntegerField()
 
 class Suppliers(models.Model):
-    suppliers_id=models.IntegerField()
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
     email=models.EmailField()
@@ -58,7 +51,6 @@ class Suppliers(models.Model):
     zip_code=models.IntegerField()
 
 class Categories_product(models.Model):
-    category_id=models.IntegerField()
     category_description=models.CharField(max_length=100)
     category_name=models.CharField(max_length=50)
 
