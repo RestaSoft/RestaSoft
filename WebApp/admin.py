7f822f1d80a3d6c1069a195c74e4590e24577a42
Alejandro Pizano <alex.pizano1234@gmail.com>
2020-11-04 10:50:43 -0700

    Agregando archivo gitignore y corrigiendolo

--- WebApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Staffs
-from .models import Products
-
-
-# Register your models here.
-
-admin.site.register(Staffs)
-admin.site.register(Products)
