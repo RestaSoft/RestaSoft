2dcfa9b4554387bc65dee9d3cdaa22ae930cb61e
Brenda Gutiérrez <blilianagr@gmail.com>
2020-11-08 01:20:50 -0700

    Commit Local

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from users import views
+from users import views as users_views
+
 
 urlpatterns = [
- # Django
-    path('registrar', views.register, name="register"),
-    path('',views.registrar),
+
+    # Users views
+    path('', users_views.login_view, name='login' ),
+    path('logout/', users_views.logout_view, name='logout'),
+    path('signup/', users_views.register, name='signup'),
 
 ] 
