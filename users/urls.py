4474d9bfe9f5c8c4896bf5d80ab7a9a3e69fb296
Fhernandez7320 <m.ferhernandez111@gmail.com>
2020-11-07 17:15:36 -0700

    add base of register

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('registrar', users_views.register, name="register"),
     path('',users_views.login, name="login"),
     path('alta',users_views.altausuario),
+    path('newuser',users_views.newuser, name="newuser"),
 
 ] 
