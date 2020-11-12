a2c34d46e7ee50f43035c90ffffed40c3dfaf89d
Brenda Gutiérrez <blilianagr@gmail.com>
2020-11-11 23:02:50 -0700

    Se modifica archivo settings, agregando cloudinary

--- RestaSoft/settings.py
+++ RestaSoft/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +45,7 @@ INSTALLED_APPS = [
     'products',
     'users',
     'orders',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +149,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 LOGIN_URL = 'login/'
+
+
