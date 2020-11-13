53ce32e230af5f454fa2494e856a65e072a4ab38
Alejandro Pizano <alex.pizano1234@gmail.com>
2020-11-12 22:09:48 -0700

    Se agregan metodos para realizar updates, se guardan cambios en el nombre del producto al actualizar.

--- products/views.py
+++ products/views.py
@@ -79,19 +79,84 @@ def delete_prod(request):
     
     return render(request,'products/delete.html')
 
+
+
+
+
+
+
+# def editar_prod(request):
+#     form = ProductsForm()
+#     if request.method == 'POST':
+#         #print(request.POST)
+#         form = ProductsForm(request.POST, request.FILES)
+
+#         if form.is_valid():
+#             try:
+#                 form.save()
+#                 return redirect("productos")
+#             except IntegrityError:
+                
+#                 return render(request, 'products/edit_products.html', {'error': 'Ya hay articulos iguales'})
+
+
+#     context = {'form':form}
+
+#     return render (request, 'products/edit_products.html', context)
+
+
+
 def editar_prod(request):
-    busqueda= request.GET["edit"]
-    #redirect to templates in templates/products
-    edit = Products.objects.filter(product_name__icontains=busqueda)
-    producto_a_modificar=edit.first()
     
-    return render (request, 'products/edit_products.html',
-    {"edit":edit,"query":busqueda,"prod_mod":producto_a_modificar})
+    if request.method=='GET':
+        busqueda= request.GET["edit"]
+         #redirect to templates in templates/products
+        edit = Products.objects.filter(product_name__icontains=busqueda)
+        producto_a_modificar=edit.first()
+    
+        return render (request, 'products/edit_products.html',{"edit":edit,"query":busqueda,"prod_mod":producto_a_modificar})
+    if request.method == 'POST':
+        id_pro= request.POST["save"]
+        nombre= request.POST["name"]
+        modify =Products.objects.get(id=id_pro)
+
+
+        modify.product_name=nombre
+        modify.save()
+        return productos(request)
+
+        print(modify)
+        #producto_a_modificar.product_name = editar
+
+        
+        # producto_a_modificar.product_name = editar.product_name
+        # producto_a_modificar.categoriesproduct=editar.categoriesproduct
+        # producto_a_modificar.list_price=editar.list_price
+        # producto_a_modificar.suppliers=editar.suppliers
+        # producto_a_modificar.image_prod=producto_a_modificar.image_prod
+
+    return render(request, 'products/edit_products.html')
+    
+        
 
 
 
 def editar(request):
-
     
     return render (request, 'products/edit_products.html')
+#     form = ProductsForm()
+#     if request.method == 'POST':
+#         #print(request.POST)
+#         form = ProductsForm(request.POST, request.FILES)
+
+#         if form.is_valid():
+#             try:
+#                 form.save()
+#                 return redirect("productos")
+#             except IntegrityError:
+                
+#                 return render(request, 'products/newproduct.html', {'error': 'Producto en existencia'})
+#     context = {'form':form}
+
+     
 
