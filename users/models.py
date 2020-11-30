4a80c7fd8bc11cf965cbddc9de92040312627be5
Brenda Gutiérrez <blilianagr@gmail.com>
2020-11-29 22:29:05 -0700

    Se modifica modelo

--- users/models.py
+++ users/models.py
@@ -50,7 +50,7 @@ class Staffs(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    phone = models.IntegerField(blank=True)
+    phone = models.BigIntegerField()
     stores = models.ForeignKey(Stores, on_delete=models.CASCADE, null=True)
     permission = models.ForeignKey(Permission, on_delete=models.CASCADE, null=True)
     active = models.BooleanField(('active'), default=True, null=True)
